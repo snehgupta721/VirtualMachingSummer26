2f976612f354890836e7424051f95469c7a90327
Sneh Gupta <sneh@kondra.com>
2026-06-15 14:08:52 -0400

    Finished exercise 2.3

--- impl/src/main/java/com/kondra/vm/MipsCPU.java
+++ impl/src/main/java/com/kondra/vm/MipsCPU.java
@@ -11,6 +11,8 @@ public class MipsCPU implements CPU {
     private final int[] registers;
     private int iPtr;
     private int nextPtr;
+    private int hi;
+    private int lo;
 
     public MipsCPU(VmThread thread, Memory systemMemory) {
         this.thread = thread;
@@ -18,16 +20,29 @@ public class MipsCPU implements CPU {
         registers = new int[32];
         iPtr = 0;
         nextPtr = 0;
+        hi = 0;
+        lo = 0;
     }
 
     @Override
     public int getRegister(int i) {
+        if (i == CPU.REG_HI) {
+            return hi;
+        } else if (i == CPU.REG_LO) {
+            return lo;
+        }
         return registers[i];
     }
 
     @Override
     public void setRegister(int i, int i1) {
-        registers[i] = i1;
+        if (i == CPU.REG_HI) {
+            hi = i1;
+        } else if (i == CPU.REG_LO) {
+            lo = i1;
+        } else {
+            registers[i] = i1;
+        }
     }
 
     @Override
@@ -105,6 +120,7 @@ public class MipsCPU implements CPU {
     private void iType(int instruction) {
         short opcode = (short) ((instruction >>> 26) & 0x3F);  // Bits 31-26
         short rs = (short) ((instruction >>> 21) & 0x1F);    // Bits 25-21
+        short rd    = (short) ((instruction >>> 11) & 0x1F); // Bits 15-11
         short rt = (short) ((instruction >>> 16) & 0x1F);    // Bits 20-16
         short immediate = (short) (instruction & 0x0000FFFF);  // Bits 15-0
         switch (opcode) {
@@ -131,6 +147,24 @@ public class MipsCPU implements CPU {
             case 0xf:
                 // lui
                 registers[rt] = immediate << 16;
+                break;
+            case 0x0a:
+                // slti
+                if (registers[rs] < immediate) {
+                    registers[rt] = 1;
+                } else {
+                    registers[rt] = 0;
+                }
+                break;
+            case 0x0b:
+                // sltiu
+                long left = ((long) registers[rs]) & 0x00000000ffffffffL;
+                long right= ((long) immediate) & 0x00000000ffffffffL;
+                if (left < right) {
+                    registers[rt] = 1;
+                } else {
+                    registers[rt] = 0;
+                }
                 break;
         }
     }
@@ -201,6 +235,70 @@ public class MipsCPU implements CPU {
             case 0x24:
                 // and
                 registers[rd] = registers[rs] & registers[rt];
+                break;
+            case 0x10:
+                // mfhi
+                registers[rd] = hi;
+                break;
+            case 0x11:
+                // mthi
+                hi = registers[rs];
+                break;
+            case 0x12:
+                // mflo
+                registers[rd] = lo;
+                break;
+            case 0x13:
+                // mtlo
+                lo = registers[rs];
+                break;
+            case 0x18:
+                // mult
+                long t = (long) registers[rs] * (long) registers[rt];
+                lo = (int) (t & 0x00000000ffffffffL);
+                hi = (int) (t >>> 32);
+                break;
+            case 0x19:
+                // multu
+                long a = ((long) registers[rs]) & 0x00000000ffffffffL;
+                long b = ((long) registers[rt]) & 0x00000000ffffffffL;
+                long product = a * b;
+                lo = (int) (product & 0x00000000ffffffffL);
+                hi = (int) (product >>> 32);
+                break;
+            case 0x1a:
+                // div
+                if (registers[rt] != 0) {
+                    lo = registers[rs] / registers[rt];
+                    hi = registers[rs] % registers[rt];
+                }
+                break;
+            case 0x1b:
+                // divu
+                long c = ((long) registers[rs]) & 0x00000000ffffffffL;
+                long d = ((long) registers[rt]) & 0x00000000ffffffffL;
+                if (registers[rt] != 0) {
+                    lo = (int) (c / d);
+                    hi = (int) (c % d);
+                }
+                break;
+            case 0x2a:
+                // slt
+                if (registers[rs] < registers[rt]) {
+                    registers[rd] = 1;
+                } else {
+                    registers[rd] = 0;
+                }
+                break;
+            case 0x2b:
+                // sltu
+                long left = ((long) registers[rs]) & 0x00000000ffffffffL;
+                long right= ((long) registers[rt]) & 0x00000000ffffffffL;
+                if (left < right) {
+                    registers[rd] = 1;
+                } else {
+                    registers[rd] = 0;
+                }
                 break;
         }
     }
